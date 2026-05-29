1f8dfd2b3c472c42998b72cb955d9a0f1d204c64
Stephan Schwab <sns@caimito.net>
2026-05-30 01:56:15 +0200

    Rename distribution branch from gh-pages to dist

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ download_binary() {
 
 get_binary_url() {
   local arch="$1"
-  echo "https://raw.githubusercontent.com/${GITHUB_OWNER}/${GITHUB_REPO}/gh-pages/downloads/${BINARY_NAME}-${arch}"
+  echo "https://raw.githubusercontent.com/${GITHUB_OWNER}/${GITHUB_REPO}/dist/downloads/${BINARY_NAME}-${arch}"
 }
 
   if [[ -n "$asset_url" ]]; then
